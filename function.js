95fe3ca34baf2b45aeb89e040c11895b56940eb5
tanim206 <mdtasnimhossaintanim206@gmail.com>
2025-10-27 14:48:22 +0600

    learn es6

---
+++ function.js
@@ -0,0 +1,92 @@
+// const obj = {
+//   name: "sadi",
+//   age: 17,
+//   class: 10,
+//   hobby: ["game", "book"],
+// };
+// console.log(obj.name);
+
+// const arr = [1,2,3,4,5,6];
+// console.log(arr);
+
+// const arrOfObj = [{name:"sadi"},{name:"mahfuz"},{name:"mhamud"},{name:"sani"}];
+
+// console.log(arrOfObj[3].name);
+
+// const arr = ['a','b']
+// arr.pop('c')
+// console.log(arr);
+  
+
+// || Or 
+// && And
+// ?  optinal changing
+
+
+// function findOddOrEven(num) {
+//     if( num%2 == 0 && num!==0){
+//        console.log("it's a even number");
+//     }else if(num%2!==0){
+//          console.log("it's a odd number");
+//     }else{
+//          console.log("it's a inifnity");
+//     }
+// }
+// findOddOrEven(0)
+
+
+// if (1==="1"){
+//     console.log("Yes, you are right");
+// }else{
+//     console.log("Nope, you are not right");
+// }
+
+
+
+// let ssc = 5.00
+// let hsc = 2.00
+// if(ssc> 4.00 && hsc > 3.00 ){
+//     console.log("you get the gift");
+// }else{
+//     console.log("you did not get the gift");
+// }
+
+
+// Filter Method
+// const arr = [1,2,3,4,5,5,6,7,8,9,10,10,10]
+// // arr.filter((n)=>{console.log(n);})
+// // const filterdNumber = arr.filter((n)=>n==desireNum)
+// // console.log(findNumber);
+
+
+// Find Method
+// const arr = [1,2,3,4,5,5,6,7,8,9,10,10,10]
+// arr.find((n)=>console.log(n))
+// let desireNum = 2 
+// const findNumber = arr.find((n)=>n==desireNum)
+// console.log(findNumber);
+
+
+// Map Method
+// const arr = [1,2,3,4,5,5,6,7,8,9,10]
+// arr.map((n)=>console.log(n))
+
+const persons = [
+  { id: 1, name: "Ayaan", age: 21, city: "Dhaka" },
+  { id: 2, name: "Mira", age: 19, city: "Chittagong" },
+  { id: 3, name: "Rafi", age: 24, city: "Sylhet" },
+  { id: 4, name: "Sohana", age: 22, city: "Rajshahi" },
+  { id: 5, name: "Nabil", age: 20, city: "Khulna" }
+];
+
+// persons.map((person)=>console.log(person))
+let query = "Digontha"
+const findPerson = persons.find((person)=>person.name==query)
+if(!findPerson){
+    console.log("user not found");
+}
+// console.log(findPerson);
+
+
+
+
